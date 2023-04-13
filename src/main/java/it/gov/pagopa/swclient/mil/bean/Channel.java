eea596a9f7a0b83ff10e9b8a8169e0f3918f988d
Antonio Tarricone <110115827+antoniotarricone@users.noreply.github.com>
2023-04-13 09:33:22 +0200

    Import organize + reformat. (#12)

--- src/main/java/it/gov/pagopa/swclient/mil/bean/Channel.java
+++ src/main/java/it/gov/pagopa/swclient/mil/bean/Channel.java
@@ -11,13 +11,13 @@ package it.gov.pagopa.swclient.mil.bean;
  */
 public class Channel {
 	public static final String ATM = "ATM";
-	
+
 	public static final String POS = "POS";
-	
+
 	public static final String TOTEM = "TOTEM";
-	
+
 	public static final String CASH_REGISTER = "CASH_REGISTER";
-	
+
 	public static final String CSA = "CSA";
 
 	private Channel() {
