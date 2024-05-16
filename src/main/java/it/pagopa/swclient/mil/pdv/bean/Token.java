3d4c19612edb57c2e817bb04fff04098e9a09802
antoniotarricone <antonio.tarricone@pagopa.it>
2024-05-16 10:10:33 +0200

    Added stuff to use Personal Data Vault.

---
+++ src/main/java/it/pagopa/swclient/mil/pdv/bean/Token.java
@@ -0,0 +1,32 @@
+/*
+ * Token.java
+ *
+ * 16 mag 2024
+ */
+package it.pagopa.swclient.mil.pdv.bean;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+/**
+ * 
+ * @author Antonio Tarricone
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Accessors(chain = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Token {
+	@JsonProperty("token")
+	private String value;
+}
