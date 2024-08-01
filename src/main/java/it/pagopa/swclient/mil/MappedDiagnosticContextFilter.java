c25e7b1c8ad5387d7a450dfc40f749a3b01e9749
antoniotarricone <antonio.tarricone@pagopa.it>
2024-08-01 12:46:34 +0200

    Used constants and added priority.

--- src/main/java/it/pagopa/swclient/mil/MappedDiagnosticContextFilter.java
+++ src/main/java/it/pagopa/swclient/mil/MappedDiagnosticContextFilter.java
@@ -10,6 +10,9 @@ import java.util.UUID;
 
 import org.jboss.logging.MDC;
 
+import it.pagopa.swclient.mil.bean.HeaderParamName;
+import jakarta.annotation.Priority;
+import jakarta.ws.rs.Priorities;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.container.PreMatching;
@@ -21,13 +24,14 @@ import jakarta.ws.rs.ext.Provider;
  */
 @Provider
 @PreMatching
+@Priority(Priorities.USER + 100)
 public class MappedDiagnosticContextFilter implements ContainerRequestFilter {
 	@Override
 	public void filter(ContainerRequestContext requestContext) throws IOException {
-		String requestId = requestContext.getHeaderString("RequestId");
+		String requestId = requestContext.getHeaderString(HeaderParamName.REQUEST_ID);
 		if (requestId == null) {
 			requestId = UUID.randomUUID().toString();
-			requestContext.getHeaders().add("RequestId", requestId);
+			requestContext.getHeaders().add(HeaderParamName.REQUEST_ID, requestId);
 		}
 		MDC.put("requestId", requestId);
 	}
