991c84cbabcf18aa64416fee62c4ae4cfdf2cd3d
Antonio Tarricone <antonio.tarricone@pagopa.it>
2024-08-05 12:30:56 +0000

    Creating site for 2.5.0

---
+++ apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"it.pagopa.swclient.mil"},{"l":"it.pagopa.swclient.mil.bean"},{"l":"it.pagopa.swclient.mil.observability"},{"l":"it.pagopa.swclient.mil.pdv.bean"},{"l":"it.pagopa.swclient.mil.pdv.client"},{"l":"it.pagopa.swclient.mil.validation.constraints"}];updateSearchResults();
