201ebb944b4fb9ab2c127dcf5ce7bc5427d337a8
antoniotarricone <antonio.tarricone@pagopa.it>
2024-08-05 15:26:47 +0200

    JavaDoc added and for "RequestId" header, constants class is used.

--- src/main/java/it/pagopa/swclient/mil/RequestIdEchoFilter.java
+++ src/main/java/it/pagopa/swclient/mil/RequestIdEchoFilter.java
@@ -7,6 +7,7 @@ package it.pagopa.swclient.mil;
 
 import java.io.IOException;
 
+import it.pagopa.swclient.mil.bean.HeaderParamName;
 import jakarta.annotation.Priority;
 import jakarta.ws.rs.Priorities;
 import jakarta.ws.rs.container.ContainerRequestContext;
@@ -15,15 +16,34 @@ import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.ext.Provider;
 
 /**
+ * <p>
  * Echoes the RequestId header from request to response.
+ * </p>
  * 
  * @author Antonio Tarricone
  */
 @Provider
 @Priority(Priorities.USER)
 public class RequestIdEchoFilter implements ContainerResponseFilter {
+	/**
+	 * <p>
+	 * Default constructor.
+	 * </p>
+	 */
+	public RequestIdEchoFilter() {
+		// Default constructor.
+	}
+	
+	/**
+	 * <p>
+	 * Echoes the RequestId header from request to response.
+	 * </p>
+	 * 
+	 * @see jakarta.ws.rs.container.ContainerResponseFilter#filter(ContainerRequestContext,
+	 *      ContainerResponseContext)
+	 */
 	@Override
 	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-		responseContext.getHeaders().add("RequestId", requestContext.getHeaderString("RequestId"));
+		responseContext.getHeaders().add(HeaderParamName.REQUEST_ID, requestContext.getHeaderString(HeaderParamName.REQUEST_ID));
 	}
 }
