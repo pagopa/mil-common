45a35bc67492ac80ffe2b52f401630baefb93f58
antoniotarricone <antonio.tarricone@pagopa.it>
2024-09-02 14:29:18 +0200

    Updated errors descriptions.

--- src/main/java/it/pagopa/swclient/mil/ErrorCode.java
+++ src/main/java/it/pagopa/swclient/mil/ErrorCode.java
@@ -107,17 +107,17 @@ public final class ErrorCode {
 
 	/**
 	 * <p>
-	 * Error description for "RequestId must not be null".
+	 * Error description for "Request ID must not be null".
 	 * </p>
 	 */
-	public static final String REQUEST_ID_MUST_NOT_BE_NULL_DESCR = "RequestId must not be null";
+	public static final String REQUEST_ID_MUST_NOT_BE_NULL_DESCR = "Request ID must not be null";
 
 	/**
 	 * <p>
-	 * Error description for "RequestId must match regexp".
+	 * Error description for "Request ID must match regexp".
 	 * </p>
 	 */
-	public static final String REQUEST_ID_MUST_MATCH_REGEXP_DESCR = "RequestId must match \"{regexp}\"";
+	public static final String REQUEST_ID_MUST_MATCH_REGEXP_DESCR = "Request ID must match \"{regexp}\"";
 
 	/**
 	 * <p>
@@ -128,17 +128,17 @@ public final class ErrorCode {
 
 	/**
 	 * <p>
-	 * Error description for "AcquirerId must not be null".
+	 * Error description for "Acquirer ID must not be null".
 	 * </p>
 	 */
-	public static final String ACQUIRER_ID_MUST_NOT_BE_NULL_DESCR = "AcquirerId must not be null";
+	public static final String ACQUIRER_ID_MUST_NOT_BE_NULL_DESCR = "Acquirer ID must not be null";
 
 	/**
 	 * <p>
-	 * Error description for "AcquirerId must match regexp".
+	 * Error description for "Acquirer ID must match regexp".
 	 * </p>
 	 */
-	public static final String ACQUIRER_ID_MUST_MATCH_REGEXP_DESCR = "AcquirerId must match \"{regexp}\"";
+	public static final String ACQUIRER_ID_MUST_MATCH_REGEXP_DESCR = "Acquirer ID must match \"{regexp}\"";
 
 	/**
 	 * <p>
@@ -156,31 +156,31 @@ public final class ErrorCode {
 
 	/**
 	 * <p>
-	 * Error description for "TerminalId must not be null".
+	 * Error description for "Terminal ID must not be null".
 	 * </p>
 	 */
-	public static final String TERMINAL_ID_MUST_NOT_BE_NULL_DESCR = "TerminalId must not be null";
+	public static final String TERMINAL_ID_MUST_NOT_BE_NULL_DESCR = "Terminal ID must not be null";
 
 	/**
 	 * <p>
-	 * Error description for "TerminalId must match regexp".
+	 * Error description for "Terminal ID must match regexp".
 	 * </p>
 	 */
-	public static final String TERMINAL_ID_MUST_MATCH_REGEXP_DESCR = "TerminalId must match \"{regexp}\"";
+	public static final String TERMINAL_ID_MUST_MATCH_REGEXP_DESCR = "Terminal ID must match \"{regexp}\"";
 
 	/**
 	 * <p>
-	 * Error description for "If Channel equals to POS, MerchantId must not be null".
+	 * Error description for "If channel equals to POS, merchant ID must not be null".
 	 * </p>
 	 */
-	public static final String MERCHANT_ID_MUST_NOT_BE_NULL_FOR_POS_DESCR = "If Channel equals to POS, MerchantId must not be null";
+	public static final String MERCHANT_ID_MUST_NOT_BE_NULL_FOR_POS_DESCR = "If channel equals to POS, merchant ID must not be null";
 
 	/**
 	 * <p>
-	 * Error description for "MerchantId must match regexp".
+	 * Error description for "Merchant ID must match regexp".
 	 * </p>
 	 */
-	public static final String MERCHANT_ID_MUST_MATCH_REGEXP_DESCR = "MerchantId must match \"{regexp}\"";
+	public static final String MERCHANT_ID_MUST_MATCH_REGEXP_DESCR = "Merchant ID must match \"{regexp}\"";
 
 	/*
 	 * Messages
@@ -188,14 +188,14 @@ public final class ErrorCode {
 
 	/**
 	 * <p>
-	 * Error message for "RequestId must not be null".
+	 * Error message for "Request ID must not be null".
 	 * </p>
 	 */
 	public static final String REQUEST_ID_MUST_NOT_BE_NULL_MSG = "[" + REQUEST_ID_MUST_NOT_BE_NULL + "] " + REQUEST_ID_MUST_NOT_BE_NULL_DESCR;
 
 	/**
 	 * <p>
-	 * Error message for "RequestId must match regexp".
+	 * Error message for "Request ID must match regexp".
 	 * </p>
 	 */
 	public static final String REQUEST_ID_MUST_MATCH_REGEXP_MSG = "[" + REQUEST_ID_MUST_MATCH_REGEXP + "] " + REQUEST_ID_MUST_MATCH_REGEXP_DESCR;
@@ -209,14 +209,14 @@ public final class ErrorCode {
 
 	/**
 	 * <p>
-	 * Error message for "AcquirerId must not be null".
+	 * Error message for "Acquirer ID must not be null".
 	 * </p>
 	 */
 	public static final String ACQUIRER_ID_MUST_NOT_BE_NULL_MSG = "[" + ACQUIRER_ID_MUST_NOT_BE_NULL + "] " + ACQUIRER_ID_MUST_NOT_BE_NULL_DESCR;
 
 	/**
 	 * <p>
-	 * Error message for "AcquirerId must match regexp".
+	 * Error message for "Acquirer ID must match regexp".
 	 * </p>
 	 */
 	public static final String ACQUIRER_ID_MUST_MATCH_REGEXP_MSG = "[" + ACQUIRER_ID_MUST_MATCH_REGEXP + "] " + ACQUIRER_ID_MUST_MATCH_REGEXP_DESCR;
@@ -237,28 +237,28 @@ public final class ErrorCode {
 
 	/**
 	 * <p>
-	 * Error message for "TerminalId must not be null".
+	 * Error message for "Terminal ID must not be null".
 	 * </p>
 	 */
 	public static final String TERMINAL_ID_MUST_NOT_BE_NULL_MSG = "[" + TERMINAL_ID_MUST_NOT_BE_NULL + "] " + TERMINAL_ID_MUST_NOT_BE_NULL_DESCR;
 
 	/**
 	 * <p>
-	 * Error message for "TerminalId must match regexp".
+	 * Error message for "Terminal ID must match regexp".
 	 * </p>
 	 */
 	public static final String TERMINAL_ID_MUST_MATCH_REGEXP_MSG = "[" + TERMINAL_ID_MUST_MATCH_REGEXP + "] " + TERMINAL_ID_MUST_MATCH_REGEXP_DESCR;
 
 	/**
 	 * <p>
-	 * Error message for "If Channel equals to POS, MerchantId must not be null".
+	 * Error message for "If channel equals to POS, merchant ID must not be null".
 	 * </p>
 	 */
 	public static final String MERCHANT_ID_MUST_NOT_BE_NULL_FOR_POS_MSG = "[" + MERCHANT_ID_MUST_NOT_BE_NULL_FOR_POS + "] " + MERCHANT_ID_MUST_NOT_BE_NULL_FOR_POS_DESCR;
 
 	/**
 	 * <p>
-	 * Error message for "MerchantId must match regexp".
+	 * Error message for "Merchant ID must match regexp".
 	 * </p>
 	 */
 	public static final String MERCHANT_ID_MUST_MATCH_REGEXP_MSG = "[" + MERCHANT_ID_MUST_MATCH_REGEXP + "] " + MERCHANT_ID_MUST_MATCH_REGEXP_DESCR;
